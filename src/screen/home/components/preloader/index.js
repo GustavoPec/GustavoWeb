fac249ef9b00a51a8051ced91a83fe39dc5740f2
GustavoPec <gustavo@softilux.com.br>
2024-03-10 23:46:06 -0300

    Ajuste Loading

--- src/screen/home/components/preloader/index.js
+++ src/screen/home/components/preloader/index.js
@@ -1,10 +1,10 @@
 import * as Styles from "./style.js";
-import Loadign from "../../../../assets/loading.png";
+import Loadign from "../../../../assets/Loading.svg";
 
 export const Preloader = () => {
   return (
     <Styles.Main>
-      <img src={Loadign} alt="logo" />
+      <img src={Loadign} alt="logo" width={300}/>
     </Styles.Main>
   );
 };
